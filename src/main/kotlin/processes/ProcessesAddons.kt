0e32db4346e40404d8c8dda2f6c1acaed7cb6bdd
ilyaorlov25 <ilya.orlov2002@yandex.ru>
2023-05-24 21:22:56 +0300

    fixed "network_mode: host"

--- src/main/kotlin/processes/ProcessesAddons.kt
+++ src/main/kotlin/processes/ProcessesAddons.kt
@@ -4,6 +4,9 @@ const val PORT_1 = 8001
 const val PORT_2 = 8002
 const val PORT_3 = 8003
 const val LOCALHOST = "localhost"
+const val NODE_1 = "node1"
+const val NODE_2 = "node2"
+const val NODE_3 = "node3"
 
 fun getNumberNode(port: Int): Int {
     return port % 1000
@@ -18,6 +21,10 @@ fun getGroupPorts(port: Int): List<Int> {
     else {
         listOf(PORT_1, PORT_2)
     }
+}
+
+fun getGroupNodes(): List<String> {
+    return listOf(NODE_1, NODE_2, NODE_3)
 }
 
 fun checkHighLevelOfProcess(process: Process):Boolean {
